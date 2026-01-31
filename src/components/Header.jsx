e4da0bcb712652f93db8c8ebd473c66a5f4385f7
Adii-tii <sableaditi8@gmail.com>
2026-01-31 08:53:29 +0530

    added logout logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,48 @@
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-function Header() {
+function Header({user, setUserDetails}) {
+  const logout = async() =>{
+      try{
+        await axios.get("http://localhost:5001/auth/logout", {withCredentials: true});
+        setUserDetails(null);
+        
+      } catch (error){
+        console.log(error);
+      }
+    }
+
   return (
     <nav className="navbar bg-dark">
       <div className="container d-flex justify-content-between">
-        {/* Brand */}
         <Link to="/" className="navbar-brand text-light">
           Expense
         </Link>
 
-        {/* Center links */}
         <div className="d-flex gap-3">
-          <Link to="/" className="nav-link text-light">
-            Home
-          </Link>
-          <Link to="/about" className="nav-link text-light">
-            About Us
-          </Link>
+          {user ? (
+            <Link to="/dashboard" className="nav-link text-light">
+              Dashboard
+            </Link>
+          ) : (
+            <>
+              <Link to="/" className="nav-link text-light">
+                Home
+              </Link>
+              <Link to="/about" className="nav-link text-light">
+                About Us
+              </Link>
+            </>
+          )}
         </div>
 
-        {/* Auth links */}
+
         <div className="d-flex gap-3">
-          <Link to="/login" className="nav-link text-light">
+          {user? <Link to="/" className="nav-link text-light" onClick={() => logout()}>
+            Log out 
+          </Link> : <Link to="/login" className="nav-link text-light">
             Login
-          </Link>
+          </Link>}
           <Link to="/register" className="nav-link text-light">
             Register
           </Link>
