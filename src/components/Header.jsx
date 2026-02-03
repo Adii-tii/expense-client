323121b7894ac9806f871be7dc63032d7133a7ea
Adii-tii <sableaditi8@gmail.com>
2026-02-03 23:49:23 +0530

    removed logout logic from main header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,6 @@
 import { Link } from "react-router-dom";
-import axios from "axios";
 
-function Header({user, setUserDetails}) {
-  const logout = async() =>{
-      try{
-        await axios.get("http://localhost:5001/auth/logout", {withCredentials: true});
-        setUserDetails(null);
-        
-      } catch (error){
-        console.log(error);
-      }
-    }
-
+function Header({user}) {
   return (
     <nav className="navbar bg-dark">
       <div className="container d-flex justify-content-between">
@@ -38,11 +27,9 @@ function Header({user, setUserDetails}) {
 
 
         <div className="d-flex gap-3">
-          {user? <Link to="/" className="nav-link text-light" onClick={() => logout()}>
-            Log out 
-          </Link> : <Link to="/login" className="nav-link text-light">
+          <Link to="/login" className="nav-link text-light">
             Login
-          </Link>}
+          </Link>
           <Link to="/register" className="nav-link text-light">
             Register
           </Link>
