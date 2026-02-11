c8f2c879aa67303a70ec3a1a60ba9faacb44c222
Adii-tii <sableaditi8@gmail.com>
2026-02-12 03:40:08 +0530

    userProfile management

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,122 +1,123 @@
-  import { useState } from "react";
-  import Button from "react-bootstrap/Button";
-  import axios from "axios";
-  import { useNavigate } from "react-router-dom";
-  import { GoogleOAuthProvider, GoogleLogin }  from "@react-oauth/google"
-  import { serverEndpoint } from "../config/appConfig.js";
-   
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google"
+import { serverEndpoint } from "../config/appConfig.js";
 
-  function Login({refreshAuth }) {  
-    const navigate = useNavigate();
-    const [formData, setFormData] = useState({
-      email: "",
-      password: ""});
 
-    const [errors, setErrors] = useState({});
-    const [message, setMessages] = useState({});
-    const handleChange = (event) => {
-      const { name, value } = event.target;
+function Login({ refreshAuth }) {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
 
-      setFormData({
-        ...formData,
-        [name]: value,
-      });
-    };
+  const [errors, setErrors] = useState({});
+  const [message, setMessages] = useState({});
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    const hasPassword = async () => {
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-      const res = await axios.post(`${serverEndpoint}/auth/valid-login`, {email: formData.email});
-      return res.data.hasPassword; //if true user has a password
+  const hasPassword = async () => {
+
+    const res = await axios.post(`${serverEndpoint}/auth/valid-login`, { email: formData.email });
+    return res.data.hasPassword; //if true user has a password
+  }
+
+  const validate = async () => {
+    let newErrors = {};
+    let isValid = true;
+
+    if (formData.email.trim() === "") {
+      newErrors.email = "Email is required";
+      isValid = false;
     }
 
-    const validate = async() => {
-      let newErrors = {};
-      let isValid = true;
+    if (formData.password.trim() === "") {
+      newErrors.password = "Password is required";
+      isValid = false;
+    }
 
-      if (formData.email.trim() === "") {
-        newErrors.email = "Email is required";
+    if (isValid) {
+      const userHasPassword = await hasPassword();
+
+      if (!userHasPassword) {
+        newErrors.general = "Please log in using Google SSO";
         isValid = false;
       }
+    }
 
-      if (formData.password.trim() === "") {
-        newErrors.password = "Password is required";
-        isValid = false;
-      }
-
-      if(isValid){
-        const userHasPassword = await hasPassword();
-
-        if(!userHasPassword){
-          newErrors.general = "Please log in using Google SSO";
-          isValid = false;
-        }
-      }
-
-     if (!isValid) {
+    if (!isValid) {
       setErrors(newErrors);
-      }
-    return isValid;
-    };
-
-    const handleFormSubmit = async (event) => {
-      event.preventDefault();
-
-      console.log("here");
-      const isValid = await validate();
-
-      if (!isValid) return;
-      console.log("passed validation")
-
-      try {
-          const body = {
-            email: formData.email,
-            password: formData.password
-          }
-
-          const res = await axios.post(`${serverEndpoint}/auth/login`, body, {withCredentials:true});
-          console.log("Login successful:", res);
-          setMessages({login: "Successfully logged in!"});
-          await refreshAuth();
-          navigate('/dashboard');
-
-      } catch (error) {
-        if (error.response) {
-          setErrors({ general: error.response.data.message});
-        } else {
-          setErrors({ general: "Network error. Please try again." });
-        }
-      } finally{
-        console.log(isValid);
-      }
-    };
-
-    const handleGoogleSuccess = async (authResponse) => {
-      console.log(JSON.stringify(authResponse));
-
-      const idToken = authResponse.credential;
-      const res = await axios.post(`${serverEndpoint}/auth/google-auth`,{idToken}, {withCredentials:true});
-      console.log("Login successful:", res);
-      setMessages({login: "Successfully logged in!"});
-      await refreshAuth(); 
-      console.log("about to navigate");
-      navigate('/dashboard');
-    };
-
-    const handleGoogleFailure = (error) => {
-      console.log(error);
     }
+    return isValid;
+  };
 
-    return (
-      <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
-          <div className="bg-white p-4 shadow-sm"
-          style={{ width: "480px" }}>
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log("here");
+    const isValid = await validate();
+
+    if (!isValid) return;
+    console.log("passed validation")
+
+    try {
+      const body = {
+        email: formData.email,
+        password: formData.password
+      }
+
+      const res = await axios.post(`${serverEndpoint}/auth/login`, body, { withCredentials: true });
+      console.log("Login successful:", res);
+      setMessages({ login: "Successfully logged in!" });
+      await refreshAuth();
+      navigate('/dashboard');
+
+    } catch (error) {
+      if (error.response) {
+        setErrors({ general: error.response.data.message });
+      } else {
+        setErrors({ general: "Network error. Please try again." });
+      }
+    } finally {
+      console.log(isValid);
+    }
+  };
+
+  const handleGoogleSuccess = async (authResponse) => {
+    console.log(JSON.stringify(authResponse));
+
+    const idToken = authResponse.credential;
+    const res = await axios.post(`${serverEndpoint}/auth/google-auth`, { idToken }, { withCredentials: true });
+    console.log("Login successful:", res);
+    setMessages({ login: "Successfully logged in!" });
+    await refreshAuth();
+    console.log("about to navigate");
+    navigate('/dashboard');
+  };
+
+  const handleGoogleFailure = (error) => {
+    console.log(error);
+  }
+
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100 bg-light">
+      <div className="bg-white p-4 shadow-sm"
+        style={{ width: "480px" }}>
         <form onSubmit={handleFormSubmit}>
           <div className="text-center mb-4">
             <h3>Welcome back!</h3>
             <p>Log in to expense</p>
           </div>
 
-          {errors.general &&   (
+          {errors.general && (
             <div className="text-danger text-center mb-3">
               {errors.general}
             </div>
@@ -161,43 +162,45 @@
             )}
           </div>
 
-          <Button type="submit" variant="success" className="w-100">
+          <Button type="submit" variant="dark" className="w-100 btn border-0 rounded-pill px-3 text-white" style={{
+            background: "#7C6CF2",
+          }}>
             Log in
           </Button>
           {message.login && (
-              <div className="text-success small mt-1 text-center">{message.login}</div>
+            <div className="text-success small mt-1 text-center">{message.login}</div>
           )}
         </form>
 
         <div className="d-flex align-items-center my-3">
-            <hr className="flex-grow-1" />
-            <span className="px-2 text-muted">or</span>
-            <hr className="flex-grow-1" />
-          </div>
+          <hr className="flex-grow-1" />
+          <span className="px-2 text-muted">or</span>
+          <hr className="flex-grow-1" />
+        </div>
 
 
         <div className="d-flex flex-column gap-2  mt-3">
-            <div className="row justify-content-center">
+          <div className="row justify-content-center">
             <div>
               <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-                <GoogleLogin onSuccess = {handleGoogleSuccess} onError= {handleGoogleFailure}/>
+                <GoogleLogin onSuccess={handleGoogleSuccess} onError={handleGoogleFailure} />
               </GoogleOAuthProvider>
-              
+
             </div>
           </div>
 
-            <div className="mt-3 text-center">
+          <div className="mt-3 text-center">
             Don't have an account?{" "}
             <a className="text-primary" href="/register">
               Register
             </a>
           </div>
 
-          </div>
-          </div>
-        
+        </div>
       </div>
-    );
-  }
 
-  export default Login;
+    </div>
+  );
+}
+
+export default Login;
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile(){
+    return (
+        <div className="mt-5 p-3">
+            <h1>User profile will be displayed here!!</h1>
+        </div>
+    )
+}
+
+export default Profile;
+
